525cf012d7368e18895edd7082c49f5e9fd3fd33
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-08 16:02:12 -0300

    colocando comandos

--- javascript/getControl.js
+++ javascript/getControl.js
@@ -1,3 +1,7 @@
+cosnt conmand = {
+  left= -1,rigth=+1,up=+1,down=-1
+};
+
 function checkControl(){
   // Colocar alguma funçao que permita perserber se possue um teclado ou gamepad conectado senao manda um aviso
 }
