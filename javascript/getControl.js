5fc3ba106ed14be1cb22d8eb1c520911c64e6b10
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-23 17:21:24 -0300

    controle

--- javascript/getControl.js
+++ javascript/getControl.js
@@ -1,5 +1,6 @@
 class controle{
-  checkControl(){
+  checkControl(event.key){
     // Colocar alguma funçao que permita perserber se possue um teclado ou gamepad conectado senao manda um aviso
+    window.addEventListener()
   }
 }
