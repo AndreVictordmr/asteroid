dd7ae2b90a41ed8faa2c7072e84cee2c86bec017
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-23 16:33:27 -0300

    Arrumando controle

--- javascript/getControl.js
+++ javascript/getControl.js
@@ -1,7 +1,5 @@
-cosnt conmand = {
-  left= -1,rigth=+1,up=+1,down=-1
-};
-
-function checkControl(){
-  // Colocar alguma funçao que permita perserber se possue um teclado ou gamepad conectado senao manda um aviso
+class controle{
+  checkControl(){
+    // Colocar alguma funçao que permita perserber se possue um teclado ou gamepad conectado senao manda um aviso
+  }
 }
